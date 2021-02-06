6007c13de683effed40b428351ad72c28a60344a
Seth Villa <sethvilla13@gmail.com>
2021-02-05 21:36:55 -0500

    mvc oop fundamentals

---
+++ MVC_Traversy/index.php
@@ -0,0 +1,6 @@
+<?php 
+// php is not a true OOP language but a procedural languange as well;
+
+// OOP adresses organization, clean, modular, and scalable code. As oposed to spaghetti procedural code;
+
+echo "Yo aqui estoy";
