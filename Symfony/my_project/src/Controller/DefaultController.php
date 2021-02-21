a4ee331236a57e367e49ce87b3ca7f11af6d19aa
Seth Villa <sethvilla13@gmail.com>
2021-02-21 17:28:53 -0500

    session cookies and request object practice

--- Symfony/my_project/src/Controller/DefaultController.php
+++ Symfony/my_project/src/Controller/DefaultController.php
@@ -4,9 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Services\GiftService;
+use PhpParser\Comment\Doc;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 // A controller is a php function that reads from the resquest object and responds with a response object 
 
@@ -14,7 +19,7 @@ class DefaultController extends AbstractController
 {   // dynamic value to route /{name}
 
     #[Route('/', name: 'default')]
-    public function index(GiftService $gifts): Response
+    public function index(GiftService $gifts, Request $request, SessionInterface $session): Response
     {
         /*
         *  render first arg mandatory View file path
@@ -28,12 +33,47 @@ class DefaultController extends AbstractController
         // Responsible for CRUD operations
         // $entityManager = $this->getDoctrine()->getManager();
 
-        // // Entity Model
+        // Entity Model
         // $user1 = new User;
         // $user2 = new User;
         // $user3 = new User;
         // $user4 = new User;
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        // $this->addFlash('notice', 'Your changes were saved');
+
+        $this->addFlash('warning', 'Your changes were not saved');
+
+        // cookies 
+        // cookie name , cookie value, duration
+        $cookie = new Cookie(
+            'my_cookie',
+            'cookie value',
+            time() + (2 * 365 * 24 * 60 * 60)
+        );
+
+        // gets data from cookies but this code seems to set sesssion cookie as well
+        // exit($request->cookies->get('PHPSESSID'));
+
+        // session set
+
+        $session->set('name', 'session value');
+        // $session->remove('name');
+
+        $session->clear();
+
+        if($session->has('name')){
+            exit($session->get('name'));
+        }
+
+
+
+        // $res = new Response();
+        // $res->headers->setCookie($cookie);
+
+        // $res->headers->clearCookie('my_cookie');
+        // $res->send();
+
 
         //    $user1->setName($users[0]);
         //    $user2->setName($users[1]);
@@ -69,5 +109,25 @@ class DefaultController extends AbstractController
         // return $this->redirectToRoute('test');
     }
 
-    
+
+
+    // requirements for Url page and default values 
+    #[Route('/blog/{page?}', name: 'blog', requirements: ['page' => '\d+'])]
+    public function blog(): Response
+    {
+        return new Response('Optional Parameters in url and requirements for parameters');
+    }
+
+    #[Route('/articles/{_locale}/{year}/{slug}/{category}', name: 'articles', defaults: ['category' => "computers"], requirements: ["_locale" => "en|fr", "category" => "computers|rtv", "year" => "\d+"])]
+    public function articles(): Response
+    {
+        return new Response('Optional Parameters in url and requirements for parameters');
+    }
+
+    // paths that determine localization https://symfony.com/doc/current/routing.html
+     #[Route(path: ["nl" => "/over-ons", "en" => "/about-us"], name: 'aboutUs')]
+     public function aboutUs(): Response
+     {
+         return new Response('Language Routes');
+     }
 }
